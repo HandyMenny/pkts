e635d62e461fd3c4a5d4451c1843165b80ed1de8
handymenny <handymenny@outlook.com>
2023-11-20 21:51:49 +0100

    revert configure kotlin

--- pkts-core/build.gradle.kts
+++ pkts-core/build.gradle.kts
@@ -2,7 +2,6 @@ import com.diffplug.spotless.LineEnding
 
 plugins {
     id("io.pkts.java-conventions")
-    kotlin("jvm") version "1.9.20"
     id("com.diffplug.spotless") version "6.22.0"
 }
 
@@ -10,14 +9,11 @@ dependencies {
     api(project(":pkts-buffers"))
     testImplementation("junit:junit:4.13.1")
     testImplementation("org.mockito:mockito-all:1.8.5")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 description = "Core Pkts"
 
 repositories { mavenCentral() }
-
-kotlin { jvmToolchain(11) }
 
 spotless {
     format("misc") {
